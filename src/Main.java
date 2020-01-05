12140c71276275c4d486c61fa7713475658849b3
Konstiantyn Havryliuk <konstiantynhavryliuk@cloud.upwork.com>
2020-01-05 22:24:39 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String str1 = "avc";
+        String str2 = "avc2";
+        String str3 = "abc" + "zxc";
+        Float a = 10.05f;
+        float b = 10.05f;
+
+        System.out.println(str3);
+
+
+        Integer i1 = 100;
+        Integer i2 = 100;
+        Integer i3 = 127;
+        Integer i4 = 127;
+        Integer i6 = 128;
+        Integer i7 = 128;
+        int i5 = 127;
+
+
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+        System.out.println(i3 == i4);
+        System.out.println(i3.equals(i4));
+    }
+}
